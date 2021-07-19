f5a9f3751d87fc8da81f727fb25b30683b9e56b7
Lukas <lukas.ancikevicius@gmail.com>
2021-07-19 11:58:45 +0300

    c selection commitment

--- src/components/commitment.jsx
+++ src/components/commitment.jsx
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 class Commitment extends Component {
   state = {};
   render() {
-    return <h1>this this commitment</h1>;
+    return (
+      <div className="commitment-component">
+        <div className="c__selection">
+          <span className="c__select">000</span>
+          <span className="c__select-title">24 men</span>
+        </div>
+        <div className="c__selection">
+          <span className="c__select">000</span>
+          <span className="c__select-title">Be isipareigojimu</span>
+        </div>
+      </div>
+    );
   }
 }
 
